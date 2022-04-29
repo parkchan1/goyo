4c2e6137eebfefc5c5c3a26040f23357746b4901
parkchan1 <103724145+parkchan1@users.noreply.github.com>
2022-04-29 14:27:53 +0900

    Add files via upload

---
+++ js/sub/sub_work_popup.js
@@ -0,0 +1,125 @@
+$(function(){
+    $(".w1 a").click(function(){
+        $(".p1").fadeIn();//show();
+        $(".p2").fadeOut();
+        $(".p3").fadeOut();
+        $(".p4").fadeOut();
+        $(".p5").fadeOut();
+        $(".p6").fadeOut();
+        $(".p7").fadeOut();
+        $(".p8").fadeOut();
+    });
+
+    $(".w2 a").click(function(){
+        $(".p1").fadeOut();
+        $(".p2").fadeIn();//show();
+        $(".p3").fadeOut();
+        $(".p4").fadeOut();
+        $(".p5").fadeOut();
+        $(".p6").fadeOut();
+        $(".p7").fadeOut();
+        $(".p8").fadeOut();
+    });
+
+    $(".w3 a").click(function(){
+        $(".p1").fadeOut();
+        $(".p2").fadeOut();
+        $(".p3").fadeIn();//show();
+        $(".p4").fadeOut();
+        $(".p5").fadeOut();
+        $(".p6").fadeOut();
+        $(".p7").fadeOut();
+        $(".p8").fadeOut();
+    });
+
+    $(".w4 a").click(function(){
+        $(".p1").fadeOut();
+        $(".p2").fadeOut();
+        $(".p3").fadeOut();
+        $(".p4").fadeIn();//show();
+        $(".p5").fadeOut();
+        $(".p6").fadeOut();
+        $(".p7").fadeOut();
+        $(".p8").fadeOut();
+    });
+
+    $(".w5 a").click(function(){
+        $(".p1").fadeOut();
+        $(".p2").fadeOut();
+        $(".p3").fadeOut();
+        $(".p4").fadeOut();
+        $(".p5").fadeIn();//show();
+        $(".p6").fadeOut();
+        $(".p7").fadeOut();
+        $(".p8").fadeOut();
+    });
+
+    $(".w6 a").click(function(){
+        $(".p1").fadeOut();
+        $(".p2").fadeOut();
+        $(".p3").fadeOut();
+        $(".p4").fadeOut();
+        $(".p5").fadeOut();
+        $(".p6").fadeIn();//show();
+        $(".p7").fadeOut();
+        $(".p8").fadeOut();
+    });
+
+    $(".w7 a").click(function(){
+        $(".p1").fadeOut();
+        $(".p2").fadeOut();
+        $(".p3").fadeOut();
+        $(".p4").fadeOut();
+        $(".p5").fadeOut();
+        $(".p6").fadeOut();
+        $(".p7").fadeIn();//show();
+        $(".p8").fadeOut();
+    });
+
+    $(".w8 a").click(function(){
+        $(".p1").fadeOut();
+        $(".p2").fadeOut();
+        $(".p3").fadeOut();
+        $(".p4").fadeOut();
+        $(".p5").fadeOut();
+        $(".p6").fadeOut();
+        $(".p7").fadeOut();
+        $(".p8").fadeIn();//show();
+        
+    });
+
+
+    $(".popup a").click(function(){
+        $(".popup").fadeOut();//hide();
+    });
+});
+
+
+
+$(function(){
+    $("#button1").click(function(){
+            $(".bestwood_main1").fadeIn();
+            $(".bestwood_main2").fadeOut();
+            $(".bestwood_main3").fadeOut();
+            $(".bestwood_main4").fadeOut();
+    });	
+    $("#button2").click(function(){
+            $(".bestwood_main1").fadeOut();
+            $(".bestwood_main2").fadeIn();
+            $(".bestwood_main3").fadeOut();
+            $(".bestwood_main4").fadeOut();
+    });
+    $("#button3").click(function(){
+            $(".bestwood_main1").fadeOut();
+            $(".bestwood_main2").fadeOut();
+            $(".bestwood_main3").fadeIn();
+            $(".bestwood_main4").fadeOut();
+    });
+    $("#button4").click(function(){
+            $(".bestwood_main1").fadeOut();
+            $(".bestwood_main2").fadeOut();
+            $(".bestwood_main3").fadeOut();
+            $(".bestwood_main4").fadeIn();
+    });
+
+});
